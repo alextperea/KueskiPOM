023e6789ea7eb7df0a9aee9185a6d4a13ad041e6
Alejandro Topete <atopete@tiempodevelopment.com>
2019-05-31 14:08:37 -0500

    create class and methods in basepage

---
+++ Interview/src/com/selenium/com/selenium/pages/BasePage.java
@@ -0,0 +1,25 @@
+package com.selenium.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	public BasePage(WebDriver driver) {
+		this.driver = driver;
+		this.wait = new WebDriverWait(driver, 10);
+	}
+	
+	public void click(WebElement element) {
+		driver.findElement(element);
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+	
+	
+}
