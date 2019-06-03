0b4335c7f18482bbb0c9919d6717386f8b1fb93e
Alejandro Topete <atopete@tiempodevelopment.com>
2019-06-03 01:37:47 -0500

    remove log4j from framework

--- Interview/src/com/selenium/com/selenium/test/FacebookInvalidCredentials.java
+++ Interview/src/com/selenium/com/selenium/test/FacebookInvalidCredentials.java
@@ -10,7 +10,6 @@ import DataProvider.provider;
 
 public class FacebookInvalidCredentials extends BaseTest {
 
-	private static Logger logger = LogManager.getLogger(FacebookInvalidCredentials.class.getName());
 	
 	@Test(dataProvider = "Facebook InvalidData", dataProviderClass = provider.class)
 	public void FacebookCredentialsTest(String googleSearch, String user, String password) {
@@ -23,7 +22,6 @@ public class FacebookInvalidCredentials extends BaseTest {
 		face.typeUser(user);
 		face.typePassword(password);
 		face.clickLogin();
-		logger.info("test");
 		
 	}
 	
