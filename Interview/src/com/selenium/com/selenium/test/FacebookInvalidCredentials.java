669a2eb493689e6871e1e8dcaa70e1ae91a569d0
Alejandro Topete <atopete@tiempodevelopment.com>
2019-06-03 01:32:25 -0500

    remove log4j from project

--- Interview/src/com/selenium/com/selenium/test/FacebookInvalidCredentials.java
+++ Interview/src/com/selenium/com/selenium/test/FacebookInvalidCredentials.java
@@ -1,8 +1,6 @@
 package com.selenium.test;
 
 import org.testng.annotations.Test;
-import org.apache.logging.log4j.*;
-
 import com.selenium.pages.FacebookHomePage;
 import com.selenium.pages.GooglePage;
 
@@ -15,15 +13,15 @@ public class FacebookInvalidCredentials extends BaseTest {
 	private static Logger logger = LogManager.getLogger(FacebookInvalidCredentials.class.getName());
 	
 	@Test(dataProvider = "Facebook InvalidData", dataProviderClass = provider.class)
-	public void FacebookCredentialsTest(String text1, String text2, String text3) {
+	public void FacebookCredentialsTest(String googleSearch, String user, String password) {
 		
 		GooglePage google = new GooglePage(driver);
-		google.typeText(text1);
+		google.typeText(googleSearch);
 		google.clickSearchButton();
 		
 		FacebookHomePage face = new FacebookHomePage(driver);
-		face.typeUser(text2);
-		face.typePassword(text3);
+		face.typeUser(user);
+		face.typePassword(password);
 		face.clickLogin();
 		logger.info("test");
 		
